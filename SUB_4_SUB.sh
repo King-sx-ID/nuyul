18c4e3fea52593cfa04c17e2fd76c43b1694c962
King-sx-ID <89509394+King-sx-ID@users.noreply.github.com>
2021-08-28 14:13:30 +0700

    Add files via upload

---
+++ SUB_4_SUB.sh
@@ -0,0 +1,523 @@
+#!bin/bash
+
+clear
+MERAH="\033[31;1m"
+HIJAU="\033[32;1m"
+
+
+figlet NUYUL YT
+echo -e $HIJAU
+
+
+cat view.txt
+
+echo "[!]------------------------------[!]"
+echo "[!] •1. Nuyul Views YT"
+echo "[1] •2. Nuyul SUBCRIBE YT"
+echo "[!] •3. Nuyul Like YT"
+echo "[!] •4. Nuyul Bot komentar"
+echo "[!] •5. Nuyul Dislike"
+echo "[!] •6. AUTO REPORT YT"
+echo "[!]------------------------------[!]"
+cat yt.txt
+
+read -p "pilih Menu Nya Ganz: " pill
+
+if [ $pill = 1 ]
+then
+clear
+cat view.txt
+cat yt.txt
+read -p "Link YouTube: " link
+read -p "Jumlah Views YT: " jum
+cat yt.txt
+echo "Jika Inggin Berhenti Ketil CTRL + C"
+cat yt.txt
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Views Succes + $jum + Berhasil [1]"
+fi
+
+						
+
+if [ $pill = 2 ]
+then
+clear
+cat view.txt
+cat yt.txt
+read -p "Link YouTube: " link
+read -p "Jumlah SUBSCRIBER: " jum
+cat yt.txt
+echo "Jika Inggin Berhenti Ketil CTRL + C"
+cat yt.txt
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul SUBSCRIBE Succes + $jum + Berhasil [1]"
+fi
+
+
+
+if [ $pill = 3 ]
+then
+clear
+cat view.txt
+cat yt.txt
+read -p "Link YouTube: " link
+read -p "Jumlah LIKES YT: " jum
+cat yt.txt
+echo "Jika Inggin Berhenti Ketil CTRL + C"
+cat yt.txt  
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul LIKE Succes + $jum + Berhasil [1]"
+fi
+
+      
+if [ $pill = 4 ]
+then
+clear
+cat view.txt
+cat yt.txt
+read -p "Link YouTube: " link
+read -p "Jumlah  Komentar: " jum
+cat yt.txt
+echo "Jika Inggin Berhenti Ketil CTRL + C"
+cat yt.txt
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul BOT COMMENT  Succes + $jum + Berhasil [1]"
+fi
+
+
+if [ $pill = 5 ]
+then
+clear
+cat view.txt
+cat yt.txt
+read -p "Link YouTube: " link
+read -p "Jumlah Dislike: " jum
+cat yt.txt
+echo "Jika Inggin Berhenti Ketil CTRL + C"
+cat yt.txt
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+sleep 1
+echo "Nuyul Dislike  Succes + $jum + Berhasil [1]"
+fi
+
+
+if [ $pill = 6 ]
+then
+clear
+cat view.txt
+cat yt.txt
+read -p "Link YouTube: " link
+read -p "Judul Konten: " judul
+read -p "Deskripsi Tambahan: " des
+read -p "Jumlah Spam Report: " spam
+cat yt.txt
+echo "Jika Inggin Berhenti Ketil CTRL + C"
+cat yt.txt
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+sleep 1
+echo "JUDUL: $judul + desk: $des + $spam + Berhasil Report"
+fi
